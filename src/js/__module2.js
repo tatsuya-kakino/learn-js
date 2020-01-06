1808d5d0ff6a3c33a2525e8d3780e2712ae8843b
tatsuya-kakino <t.kakino1002@gmail.com>
2020-01-07 03:22:21 +0900

    object-tree-view[wip]

--- src/js/__module2.js
+++ src/js/__module2.js
@@ -1,18 +1,18 @@
 'use strict'
 // let variable1 = "fromModule.js";
 
-export const module2 = {
-  variable: "fromModule2.js",
+// export const module2 = {
+//   variable: "fromModule2.js",
 
-  print() {
-    console.log(this.variable)
-  }
-}
-export function name2(){
-  const print = (function(){
-    const name = "kaikino2"
-    console.log(name)
-  })();
-  const name = "name2"
-  return name
-}
+//   print() {
+//     console.log(this.variable)
+//   }
+// }
+// export function name2(){
+//   const print = (function(){
+//     const name = "kaikino2"
+//     console.log(name)
+//   })();
+//   const name = "name2"
+//   return name
+// }
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-// 'use strict'
+'use strict'
 import '../css/style.scss';
-import { Count } from './__Counter';
+import { Count } from './Counter';
 
 const OBJECT = () => {
   const OBJ = {
@@ -10,7 +10,13 @@ const OBJECT = () => {
       child2: "child2",
       child3: {
         grandChild1: "grandChild1",
-        grandChild2: "grandChild2"
+        grandChild2: "grandChild2",
+        grandChild3: {
+          grandChild4: "grandChild4",
+          grandChild5: {
+            grandChild6: "grandChild6"
+          }
+        }
       }
     },
     parent3: "parent3",
@@ -22,6 +28,32 @@ const OBJECT = () => {
   })();
 };
 const DOM = document.querySelector(".wrapper");
+
+/**
+ * 引数Elementに渡したDOMをメソッド名のElementでWrapする
+ */
+class Wrapper {
+  static Div(Element) {
+    const WRAPPER = document.createElement('div')
+    Element.before(WRAPPER);
+    WRAPPER.append(Element);
+  }
+  static Ul() {
+    const WRAPPER = document.createElement('ul')
+    Element.before(WRAPPER);
+    WRAPPER.append(Element);
+  }
+  static li() {
+    const WRAPPER = document.createElement('li')
+    Element.before(WRAPPER);
+    WRAPPER.append(Element);
+  }
+  static p() {
+    const WRAPPER = document.createElement('p')
+    Element.before(WRAPPER);
+    WRAPPER.append(Element);
+  }
+}
 
 class TreeView {
   constructor(OBJECT, DOM){
@@ -37,8 +69,8 @@ class TreeView {
     }
   }
 }
-const Render = (OBJECT, DOM) => {
-  const count_int = new Count(0).Increment();
+const Render = (OBJECT, DOM, COUNT) => {
+  const count_int = new Count(COUNT).Increment();
   const tree_obj = new TreeView(OBJECT, DOM);
   const objArray = Object.entries(OBJECT);
   for(let n of objArray){
@@ -46,41 +78,43 @@ const Render = (OBJECT, DOM) => {
     if(objBool){
       const key = n[0];
       console.log(key);
-      Render(n[1], DOM);
+      console.log(`NEST-COUNT: ${COUNT}`);
+      Render(n[1], DOM, new Count(count_int).get());
     }else{
       const key = n[1];
       console.log(key);
+      console.log(`NEST-COUNT: ${COUNT}`);
     }
   }
 }
-Render(OBJECT(), DOM);
+Render(OBJECT(), DOM, new Count(0).get());
 
-const NULL     = null;
-const ob       = {parent: "parent"};
-const arr      = [1, 2, 3, 4, 5];
-const date     = new Date();
-const fn       = () => {};
-const str      = "string!";
-const num      = 123;
-const bool     = true;
-const undefine = undefined;
+// const NULL     = null;
+// const ob       = {parent: "parent"};
+// const arr      = [1, 2, 3, 4, 5];
+// const date     = new Date();
+// const fn       = () => {};
+// const str      = "string!";
+// const num      = 123;
+// const bool     = true;
+// const undefine = undefined;
 
-console.log(`instanceof null:      ${NULL instanceof Object}`)    //instanceof null:      false
-console.log(`instanceof object:    ${ob instanceof Object}`)      //instanceof object:    true
-console.log(`instanceof array:     ${arr instanceof Object}`)     //instanceof array:     true
-console.log(`instanceof instanse:  ${date instanceof Object}`)    //instanceof instanse:  true
-console.log(`instanceof function:  ${fn instanceof Object}`)      //instanceof function:  true
-console.log(`instanceof string:    ${str instanceof Object}`)     //instanceof string:    false
-console.log(`instanceof number:    ${num instanceof Object}`)     //instanceof number:    false
-console.log(`instanceof boolean:   ${bool instanceof Object}`)    //instanceof boolean:   false
-console.log(`instanceof undefined: ${undefine instanceof Object}`)//instanceof undefined: false
+// console.log(`instanceof null:      ${NULL instanceof Object}`)    //instanceof null:      false
+// console.log(`instanceof object:    ${ob instanceof Object}`)      //instanceof object:    true
+// console.log(`instanceof array:     ${arr instanceof Object}`)     //instanceof array:     true
+// console.log(`instanceof instanse:  ${date instanceof Object}`)    //instanceof instanse:  true
+// console.log(`instanceof function:  ${fn instanceof Object}`)      //instanceof function:  true
+// console.log(`instanceof string:    ${str instanceof Object}`)     //instanceof string:    false
+// console.log(`instanceof number:    ${num instanceof Object}`)     //instanceof number:    false
+// console.log(`instanceof boolean:   ${bool instanceof Object}`)    //instanceof boolean:   false
+// console.log(`instanceof undefined: ${undefine instanceof Object}`)//instanceof undefined: false
 
-console.log(`typeof null:      ${typeof NULL}`)    //typeof null:      object
-console.log(`typeof object:    ${typeof ob}`)      //typeof object:    object
-console.log(`typeof array:     ${typeof arr}`)     //typeof array:     object
-console.log(`typeof instanse:  ${typeof date}`)    //typeof instanse:  object
-console.log(`typeof function:  ${typeof fn}`)      //typeof function:  function
-console.log(`typeof string:    ${typeof str}`)     //typeof string:    string
-console.log(`typeof number:    ${typeof num}`)     //typeof number:    number
-console.log(`typeof boolean:   ${typeof bool}`)    //typeof boolean:   boolean
-console.log(`typeof undefined: ${typeof undefine}`)//typeof undefined: undefined
+// console.log(`typeof null:      ${typeof NULL}`)    //typeof null:      object
+// console.log(`typeof object:    ${typeof ob}`)      //typeof object:    object
+// console.log(`typeof array:     ${typeof arr}`)     //typeof array:     object
+// console.log(`typeof instanse:  ${typeof date}`)    //typeof instanse:  object
+// console.log(`typeof function:  ${typeof fn}`)      //typeof function:  function
+// console.log(`typeof string:    ${typeof str}`)     //typeof string:    string
+// console.log(`typeof number:    ${typeof num}`)     //typeof number:    number
+// console.log(`typeof boolean:   ${typeof bool}`)    //typeof boolean:   boolean
+// console.log(`typeof undefined: ${typeof undefine}`)//typeof undefined: undefined
